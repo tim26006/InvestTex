fb49c57690aedbaab9f119ee965ba5f2c0db2c5a
B2ng4 <kostyan2004tt@mail.ru>
2024-06-06 13:32:41 +1000

    dialog

--- frontend/src/components/Input_screan.jsx
+++ frontend/src/components/Input_screan.jsx
@@ -1,20 +1,45 @@
-import { useState } from 'react'
-import { Input, Card, Space } from "antd";
-
+import React, { useState } from 'react';
+import { Input, Card } from "antd";
 import send_img from './img/send.png';
 
-
 function Input_screan() {
-    const [count, setCount] = useState(0)
-  
+    const [inputValue, setInputValue] = useState('');
+    const [messages, setMessages] = useState([]);
+
+    const handleChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
+    const handleClick = () => {
+        // Добавляем новое сообщение к списку сообщений
+        const newMessage = { text: inputValue, id: Date.now() };
+        setMessages([...messages, newMessage]);
+        // Очищаем поле ввода после отправки
+        setInputValue('');
+    };
+
     return (
-      <div className='input'>
-         <Input placeholder="Опишите, что вам необходимо...." style={{
-        fontWeight: 'bold'
-        }} />
-         <button className='send_btn'>  <img className='send_img' src={send_img} alt="Your Image" /></button>
-      </div>
-    )
-  }
-  
-  export default Input_screan
+        <div className='input'>
+            <div className="messages">
+                {messages.map(message => (
+                    <Card key={message.id} style={{ marginBottom: 10 }}>
+                        <p>{message.text}</p>
+                    </Card>
+                ))}
+            </div>
+            <div className="input-wrapper">
+                <Input
+                    placeholder="Опишите, что вам необходимо...."
+                    style={{ fontWeight: 'bold' }}
+                    value={inputValue}
+                    onChange={handleChange}
+                />
+                <button className='send_btn' onClick={handleClick}>
+                    <img className='send_img' src={send_img} alt="Send" />
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Input_screan;
