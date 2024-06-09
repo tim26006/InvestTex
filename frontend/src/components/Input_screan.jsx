d6dcb50ca20085a79ecb5d3a4178301a61d7267b
B2ng4 <kostyan2004tt@mail.ru>
2024-06-09 17:34:02 +1000

    ModalMap

--- frontend/src/components/Input_screan.jsx
+++ frontend/src/components/Input_screan.jsx
@@ -1,13 +1,11 @@
+// InputScreen.js
 import React, { useState, useEffect, useRef } from 'react';
-import { Input, Card, Space, Alert, Pagination  } from "antd";
+import { Input, Card, Space, Alert } from "antd";
 import axios from 'axios';
 import Modal from 'react-modal';
 import { IoMdHelp } from "react-icons/io";
 import { FiSend } from "react-icons/fi";
-import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
-
-
-
+import MapModal from './ModalMap';
 
 function InputScreen() {
     const [inputValue, setInputValue] = useState('');
@@ -68,23 +66,6 @@ function InputScreen() {
         setInputValue('');
     };
 
-
-    const PannellumViewer = ({ imageUrl }) => {
-        const pannellumContainer = useRef(null);
-      
-        useEffect(() => {
-          pannellum.viewer(pannellumContainer.current, {
-            type: 'equirectangular',
-            panorama: imageUrl,
-            autoLoad: true,
-          });
-        }, [imageUrl]);
-      
-        return <div ref={pannellumContainer} style={{ width: '100%', height: '500px' }} />;
-      };
-
-
-
     return (
         <div className='input'>
             <div className="messages">
@@ -136,37 +117,8 @@ function InputScreen() {
                 </p>
             </Modal>
 
-                        <Modal isOpen={isMapModalOpen} onRequestClose={closeMapModal} className="modalMap">
-                <h1 className='title_map'>Название</h1>
-                <div className="description-block">
-                    <p className="descriptionMap">Характеристики</p>
-                </div>
-                <YMaps>
-                    <Map
-                        defaultState={{
-                            center: [55.75, 37.57],
-                            zoom: 9,
-                            controls: ["zoomControl", "fullscreenControl"],
-                        }}
-                        modules={["control.ZoomControl", "control.FullscreenControl"]}
-                    >
-                        <Placemark
-                            modules={["geoObject.addon.balloon"]}
-                            defaultGeometry={[55.75, 37.57]}
-                        />
-                    </Map>
-                </YMaps>
-                <div style={{ position: 'relative', overflow: 'hidden' }}>
-                    <a href="https://yandex.ru/maps/213/moscow/?utm_medium=mapframe&utm_source=maps" style={{ color: '#eee', fontSize: '12px', position: 'absolute', top: '0px' }}>Москва</a>
-                    <a href="https://yandex.ru/maps/213/moscow/geo/krasnaya_ploshchad/1520633025/?indoorLevel=1&ll=37.620941%2C55.753575&panorama%5Bdirection%5D=326.893977%2C1.000000&panorama%5Bfull%5D=true&panorama%5Bid%5D=1298161349_673227531_23_1443042000&panorama%5Bpoint%5D=37.621255%2C55.753498&panorama%5Bspan%5D=117.022349%2C60.000000&tab=panorama&utm_medium=mapframe&utm_source=maps&z=17" style={{ color: '#eee', fontSize: '12px', position: 'absolute', top: '14px' }}>Красная площадь в городе Москва — Яндекс Карты</a>
-                    <iframe src="https://yandex.ru/map-widget/v1/?indoorLevel=1&ll=37.620941%2C55.753575&mode=search&ol=geo&ouri=ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgoxNTIwNjMzMDI1EjnQoNC-0YHRgdC40Y8sINCc0L7RgdC60LLQsCwg0JrRgNCw0YHQvdCw0Y8g0L_Qu9C-0YnQsNC00YwiCg0dfBZCFaIDX0I%2C&panorama%5Bdirection%5D=326.893977%2C1.000000&panorama%5Bfull%5D=true&panorama%5Bid%5D=1298161349_673227531_23_1443042000&panorama%5Bpoint%5D=37.621255%2C55.753498&panorama%5Bspan%5D=117.022349%2C60.000000&tab=panorama&z=17" width="560" height="400" frameBorder="1" allowFullScreen="true" style={{ position: 'relative' }}></iframe>
-                </div>
-                <div className="pagination-center">
-                    <Pagination defaultCurrent={1} total={30} />
-                </div>
-            </Modal>
+            <MapModal isOpen={isMapModalOpen} onRequestClose={closeMapModal} />
 
-                
             {error && (
                 <div className="centered-error">
                     <Space direction="vertical" style={{ width: '100%' }}>
