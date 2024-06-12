5cc6dd3b844cf0f80da12ad43acf0ea680c9bbff
B2ng4 <kostyan2004tt@mail.ru>
2024-06-12 16:46:44 +1000

    логин

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -3,9 +3,14 @@ import { Input, Button } from 'antd';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate function
 import { Navigate } from 'react-router-dom'; // Import Navigate component
+import './Login.css';
+import { Spin, Alert } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
 
 function Login() {
   const navigate = useNavigate(); // Use the useNavigate function to redirect users
+  const antIcon = <LoadingOutlined style={{ fontSize: 100, color: 'red' }} spin />;
+  const [isLoading, setIsLoading] = useState(false);
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -13,6 +18,7 @@ function Login() {
 
   const handleLogin = async () => {
     try {
+      setIsLoading(true);
       const response = await axios.post('http://localhost:8000/api/login', { email, password });
       const { access_token } = response.data; // Extract access_token from response
       localStorage.setItem('access_token', access_token); // Store access_token in localStorage
@@ -20,9 +26,14 @@ function Login() {
       navigate('/personal');
     } catch (error) {
       console.error('Error while making the request:', error);
+      if (error.response.status === 400){
+        <Alert message="Введены неверные данные" type="warning" />
+      }
       // Handle login error
-    }
-  };
+    } finally {
+    setIsLoading(false); // Установите isLoading в false после завершения авторизации
+  }
+};
 
   // If isAuthenticated is true, redirect the user to PersonalArea
   if (isAuthenticated) {
@@ -30,16 +41,24 @@ function Login() {
   }
 
   return (
-    <div>
-      <h1>Login</h1>
+    <div className='Window_login'>
+      <h1>Вход</h1>
       <div>
-        <Input placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
+      <div className='text'>
+        <p>Возможность для зарегистрированных пользователей:</p>
+        <li>Сохранять историю запросов</li>
+        <li className='double_li'>Скачивать полные отчеты</li>
+      </div>
+        <Input placeholder="Логин" value={email} onChange={e => setEmail(e.target.value)} />
       </div>
       <div>
-        <Input.Password placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+        <Input  type="password"  placeholder="Пароль" value={password} onChange={e => setPassword(e.target.value)} />
       </div>
       <div>
-        <Button type="primary" onClick={handleLogin}>Login</Button>
+      <Button type="primary" onClick={handleLogin}>
+          {isLoading ? <Spin indicator={antIcon} /> : 'Вход'} 
+        </Button>
+      <div className='url'><a href='/reg'>Еще не регистрировались?</a></div>
       </div>
     </div>
   );
