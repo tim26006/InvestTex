8da490ebb23e67bafaf858813a2fe9c0f55c08f5
B2ng4 <kostyan2004tt@mail.ru>
2024-06-14 16:09:11 +1000

    гр

--- frontend/src/components/Input_screan.jsx
+++ frontend/src/components/Input_screan.jsx
@@ -5,6 +5,7 @@ import Modal from 'react-modal';
 import { IoMdHelp } from "react-icons/io";
 import { FiSend } from "react-icons/fi";
 import MapModal from './ModalMap';
+import ModalLgots from './ModalLgots';
 
 function InputScreen() {
     const [inputValue, setInputValue] = useState('');
@@ -12,13 +13,13 @@ function InputScreen() {
     const [isHelpModalOpen, setIsHelpModalOpen] = useState(false);
     const [isMapModalOpen1, setIsMapModalOpen1] = useState(false);
     const [isMapModalOpen2, setIsMapModalOpen2] = useState(false);
-    const [isMapModalOpen3, setIsMapModalOpen3] = useState(false);
     const [error, setError] = useState(false);
     const [mapData, setMapData] = useState(null);
     const [mapData1, setMapData1] = useState(null);
     const [mapData2, setMapData2] = useState(null);
     const [isDelayMessageSent, setIsDelayMessageSent] = useState(false);
-    const [isLoading, setIsLoading] = useState(false); // Состояние для отслеживания загрузки
+    const [isLoading, setIsLoading] = useState(false);
+    const [lgotData, setLgotData] = useState(null);
 
     const messagesEndRef = useRef(null);
 
@@ -44,14 +45,6 @@ function InputScreen() {
         setIsHelpModalOpen(false);
     };
 
-    const openMapModal = () => {
-        setIsMapModalOpen(true);
-    };
-
-    const closeMapModal = () => {
-        setIsMapModalOpen(false);
-    };
-
     const handleChange = (e) => {
         setInputValue(e.target.value);
     };
@@ -63,7 +56,7 @@ function InputScreen() {
         }
         setError(false);
 
-        setIsLoading(true); // Устанавливаем состояние загрузки перед отправкой запроса
+        setIsLoading(true);
 
         const newMessage = { text: inputValue, source: "Вы" };
         setMessages([...messages, newMessage]);
@@ -80,7 +73,7 @@ function InputScreen() {
             }
         })
         .then(response => {
-            setIsLoading(false); // Сбрасываем состояние загрузки после получения ответа
+            setIsLoading(false);
             const responseData = response.data;
 
             if (Array.isArray(responseData)) {
@@ -92,7 +85,7 @@ function InputScreen() {
             }
         })
         .catch(error => {
-            setIsLoading(false); // Сбрасываем состояние загрузки в случае ошибки
+            setIsLoading(false);
             console.error(error);
         });
 
@@ -100,41 +93,49 @@ function InputScreen() {
     };
 
     const handleResponse = (data) => {
-        console.log("Response data:", data); // Log the response data to the console
-        
-        const message = { text: data.response, source: "Бот" };
-        setMessages(prevMessages => [...prevMessages, message]);
-        
-        if (data.last === "True") {
-            axios.post('http://127.0.0.1:8000/api/get-platforms', { withCredentials: true }, {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-            .then(response => {
-                const responseData = response.data;
-
-                if (Array.isArray(responseData)) {
-                    responseData.forEach(data => {
-                        handleResponse(data);
-                    });
-                } else {
-                    handleResponse(responseData);
-                }
-            })
-            .catch(error => {
-                console.error(error);
-            });
+        console.log("Response data:", data);
+    
+        // Проверяем значение lgots
+        if (data.lgots === 1) {
+            try {
+                const responseData = JSON.parse(data.response);
+    
+                // Устанавливаем данные льгот для последующего использования
+                setLgotData(responseData);
+    
+                // Отправляем только кнопку "Посмотреть льготы"
+                setMessages(prevMessages => [...prevMessages, { text: "Вот какие льготы", isButton: true, source: "Бот" }]);
+            } catch (error) {
+                console.error("Error parsing JSON from response:", error);
+            }
+        } else {
+            // Добавляем сообщение в messages только если lgots не равен 1
+            const message = { text: data.response, source: "Бот" };
+            setMessages(prevMessages => [...prevMessages, message]);
+    
+            if (data.text === "Нашел несколько площадок для Вас") {
+                setIsMapModalOpen1(true);
+            }
         }
+    
+    
 
         try {
             const jsonData = JSON.parse(data.features);
             const answer = JSON.parse(data.answer2);
             const answer2 = JSON.parse(data.answer3);
+
             if (jsonData !== "нема") {
                 setMapData(jsonData);
                 setMapData1(answer);
                 setMapData2(answer2);
+            }
+
+            if (data.lgots === 1) {
+
+                setMessages(prevMessages => [...prevMessages, { text: data.response, isButton: true, source: "Бот" }]);
+            } else if (data.text === "Нашел несколько площадок для Вас") {
+                setIsMapModalOpen1(true);
             }
         } catch (error) {
             console.error("Error parsing JSON data:", error);
@@ -142,7 +143,6 @@ function InputScreen() {
     };
 
     return (
-
         <div className='input'>
             <div className="messages">
                 {messages.length === 0 ? (
@@ -151,12 +151,15 @@ function InputScreen() {
                     messages.map((message, index) => (
                         <Card key={index} style={{ marginBottom: 10 }}>
                             <p className={`message-label ${message.source === "Бот" ? 'bot' : ''}`}>{message.source}</p>
-                            <p style={{ fontSize: 18 }}>
-                                {message.text}
-                            </p>
+                            <p style={{ fontSize: 18 }}>{message.text}</p>
+                            {message.isButton && message.text === "Вот какие льготы" && (
+                                 <div className='button_check_offer'>
+                                <button onClick={() => setIsMapModalOpen2(true)}>Посмотреть льготы</button>
+                                </div>
+                            )}
                             {message.text === "Нашел несколько площадок для Вас" && (
                                 <div className='button_check_offer'>
-                                    <button onClick={() => setIsMapModalOpen1(true)}>Посмотреть</button>
+                                    <button onClick={() => setIsMapModalOpen1(true)}>Посмотреть объекты</button>
                                 </div>
                             )}
                         </Card>
@@ -197,6 +200,7 @@ function InputScreen() {
             </Modal>
 
             <MapModal isOpen={isMapModalOpen1} onRequestClose={() => setIsMapModalOpen1(false)} mapData={mapData} mapDataone={mapData1} mapDatatwo={mapData2}/>
+            <ModalLgots isOpen={isMapModalOpen2} onRequestClose={() => setIsMapModalOpen2(false)} data={lgotData}/> 
 
             {error && (
                 <div className="centered-error">
@@ -206,9 +210,8 @@ function InputScreen() {
                 </div>
             )}
 
-            {/* Спиннер на весь экран во время загрузки */}
             <Spin spinning={isLoading} size="large" tip="Отправка сообщения...">
-                {/* Весь ваш текущий JSX здесь */}
+                {/* Здесь может быть дополнительный JSX, который отображается во время загрузки */}
             </Spin>
         </div>
     );
---
+++ frontend/src/components/ModalLgots.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Modal } from 'antd';
+import './ModalLgots.css';
+import { Button } from 'antd';
+import { FaCity } from "react-icons/fa6";
+
+
+
+
+function ModalLgots({ isOpen, onRequestClose, data }) {
+  const reg = data ? data["Регион"] : "Ничего";
+  const name = data ? data["Наименование меры поддержки"] : 'Название';
+  let sytb = data ? data["Суть механизма"] : 'Суть';
+  sytb = sytb.replace(/&nbsp;/g, '');
+  const URL = data ? data["Ссылка на форму подачи заявки"] : "URL";
+
+  return (
+    <Modal
+      open={isOpen}
+      onCancel={onRequestClose}
+      footer={null}
+      closable={false}
+      width={600} // Устанавливаем ширину модального окна
+      style={{ top: 20 }} // Устанавливаем отступ сверху и стили модального окна
+      className="custom-modal" // Добавляем кастомный класс
+    >
+      <div className='TEXT_LGOTS'>
+        <div className='title_Modallog'><FaCity size={30} color={'#ef0f33'}/> {reg}</div>
+        <div>Название: {name}.</div>
+        <div>Суть: {sytb}.</div>
+        <Button 
+            type="primary" 
+            href={URL} 
+            target="_blank" // Открывать ссылку в новом окне
+            className="custom-button"
+            rel="noopener noreferrer" // Рекомендуется добавить для безопасности
+          >
+            Перейти
+          </Button>
+      </div>
+    </Modal>
+  );
+}
+
+export default ModalLgots;
